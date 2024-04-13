ec18c0b56fbc2354d37a1dc108f99191b119d8da
Mateo Lizcano <mateodevcode@gmail.com>
2024-04-13 02:20:21 +0200

    Agregando la carpeta de los componentes

---
+++ src/components/HistoryDeploy.jsx
@@ -0,0 +1,11 @@
+import HistoryExp from "./HistoryExp"
+
+function HistoryDeploy() {
+  return (
+    <div>
+        <HistoryExp />
+    </div>
+  )
+}
+
+export default HistoryDeploy
---
+++ src/components/HistoryExp.jsx
@@ -0,0 +1,9 @@
+function HistoryExp() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HistoryExp
