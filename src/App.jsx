4a699dcab7cc024a0a79442df7313ddd7ab25f9a
Mateo Lizcano <mateodevcode@gmail.com>
2024-04-13 23:00:40 +0200

    Se actualizo el archivo App.jsx y Skills.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import HistoryDeploy from "./components/HistoryDeploy";
+import Skills from "./components/Skills";
 import Target from "./components/Target";
 import Tittle from "./components/Tittle";
 
 function App() {
   return (
-    <div className="flex flex-col justify-start items-center ">
+    <div className="flex flex-col justify-start items-center px-80">
       <div className="w-full">
         <Tittle />
         <Target />
         <HistoryDeploy />
+        <Skills />
       </div>
     </div>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,51 @@
 function Skills() {
   return (
-    <div className="px-10">
-      <h3 className="font-semibold text-3xl">SKILLS</h3>
-      <div className="w-full">
-        <div className="border-violet-500 border-2 rounded-xl w-auto">
+    <div className="px-10 w-full">
+      <h3 className="font-semibold text-3xl hover:text-blue-700 w-min">SKILLS</h3>
+      <div className="w-full flex flex-wrap">
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
+            React
+        </div>
+        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
             React
         </div>
       </div>
