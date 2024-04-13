dc6bdf2eb7d564ea467ba4c9c81cec007a26f669
Mateo Lizcano <mateodevcode@gmail.com>
2024-04-13 23:49:43 +0200

    se actualizo el componente Skills.jsx y se agrego la imagen js.png

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,13 @@
+import javascript from '../img/js.png';
+
 function Skills() {
   return (
-    <div className="px-10 w-full">
-      <h3 className="font-semibold text-3xl hover:text-blue-700 w-min">SKILLS</h3>
+    <div className="px-10 w-full mt-20">
+      <h3 className="font-semibold text-3xl hover:text-blue-800 w-min pb-4">SKILLS</h3>
       <div className="w-full flex flex-wrap">
-        <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
-            React
+        <div className="flex flex-row jus items-center border-blue-700 hover:border-green-600 border-2 rounded-xl w-max px-3 py-1 m-1.5 font-semibold cursor-pointer">
+            <img src={javascript} alt="Javascript" className='w-5 mr-3' />
+            <span>Javascript</span>
         </div>
         <div className="border-blue-700 hover:border-green-600 border-2 rounded-xl w-min px-3 py-0.5 m-1.5 font-semibold cursor-pointer">
             React
