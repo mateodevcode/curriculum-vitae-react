9fbcfcdc208d68bd874a7f98f1b122d2cb193394
Rey <reydavidquiroz@gmail.com>
2024-04-13 16:33:11 -0500

    update Tittle and target

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -2,58 +2,59 @@ import React from "react";
 
 function Target() {
   return (
-    <div className="t-main flex flex-col justify-start items-center bg-gray-950 border-black border-2 rounded-xl mx-80 mt-8 w-80 h-auto p-16 pb-2 text-gray-100 ">
-      
-      <div className="t-descripcion text-xs bg-gray-800 rounded-md p-1">
-        Software developer
-      </div>
-      <div className="t-box flex  m-4">
-        <div className="t-face  bg-gray-800 rounded-md mx-1 text-transparent w-7 h-7" >
-          face
+    <div className="pl-10">
+      <div className="t-main flex flex-col justify-start items-center bg-gray-950 border-black border-2 rounded-xl  mt-8 w-80 h-auto p-16 pb-2 text-gray-100 ">
+        <div className="t-descripcion text-xs bg-gray-800 rounded-md p-1">
+          Software developer
         </div>
-        <div className="t-twitter  bg-gray-800 rounded-md mx-1 text-transparent w-7 h-7">
-          twit
-        </div>
-        <div className="t-instagram  bg-gray-800 rounded-md mx-1 text-transparent w-7 h-7 ">
-          inst
-        </div>
-        <div className="t-instagram  bg-gray-800 rounded-md mx-1 text-transparent w-7 h-7">
-          link
-        </div>
-      </div>
-      <div className="t-box-2 items-start bg-gray-800 rounded-md w-60">
-        <div className="t-phone flex flex-col justify-center items-left  border-gray-700 border-b-2 mt-4 text-xs pb-2">
-          <p className=" absolute pl-3 text-xl ">📱</p>
-          <div className="pl-12">
-            <h3>Phone</h3>
-            <p>3043819339</p>
+        <div className="t-box flex  m-4">
+          <div className="t-face  bg-gray-800 rounded-md mx-1 text-transparent w-7 h-7">
+            face
+          </div>
+          <div className="t-twitter  bg-gray-800 rounded-md mx-1 text-transparent w-7 h-7">
+            twit
+          </div>
+          <div className="t-instagram  bg-gray-800 rounded-md mx-1 text-transparent w-7 h-7 ">
+            inst
+          </div>
+          <div className="t-instagram  bg-gray-800 rounded-md mx-1 text-transparent w-7 h-7">
+            link
           </div>
         </div>
-        <div className="t-email flex flex-col justify-center items-left border-gray-700 border-b-2 text-xs py-2">
-          <p className=" absolute pl-3 text-xl ">📧</p>
-          <div className="pl-12">
-            <h3>Email</h3>
-            <p>reydavidquiroz@gmail.com</p>
+        <div className="t-box-2 items-start bg-gray-800 rounded-md w-60">
+          <div className="t-phone flex flex-col justify-center items-left  border-gray-700 border-b-2 mt-4 text-xs pb-2">
+            <p className=" absolute pl-3 text-xl ">📱</p>
+            <div className="pl-12">
+              <h3>Phone</h3>
+              <p>3043819339</p>
+            </div>
+          </div>
+          <div className="t-email flex flex-col justify-center items-left border-gray-700 border-b-2 text-xs py-2">
+            <p className=" absolute pl-3 text-xl ">📧</p>
+            <div className="pl-12">
+              <h3>Email</h3>
+              <p>reydavidquiroz@gmail.com</p>
+            </div>
+          </div>
+          <div className="t-direccion flex flex-col justify-center items-left border-gray-700 border-b-2 text-xs py-2">
+            <p className=" absolute  pl-3 text-xl ">📍</p>
+            <div className="pl-12">
+              <h3>Direccion</h3>
+              <p>TV 35 29a-49sur</p>
+            </div>
+          </div>
+          <div className="t-birthday flex flex-col justify-center items-left mb-4 text-xs pt-2">
+            <p className=" absolute pl-3 text-xl ">🎂</p>
+            <div className="pl-12">
+              <h3>Birthday</h3>
+              <p>28/02/1997</p>
+            </div>
           </div>
         </div>
-        <div className="t-direccion flex flex-col justify-center items-left border-gray-700 border-b-2 text-xs py-2">
-          <p className=" absolute  pl-3 text-xl ">📍</p>
-          <div className="pl-12">
-            <h3>Direccion</h3>
-            <p>TV 35 29a-49sur</p>
-          </div>
+        <div className="t-linkedin bg-blue-600 rounded-md text-sm text-gray-100 p-1 mt-6 mb-4">
+          Descargar CV
         </div>
-        <div className="t-birthday flex flex-col justify-center items-left mb-4 text-xs pt-2">
-          <p className=" absolute pl-3 text-xl ">🎂</p>
-          <div className="pl-12">
-            <h3>Birthday</h3>
-            <p>28/02/1997</p>
-          </div>
-        </div>
-      </div>
-      <div className="t-linkedin bg-blue-600 rounded-md text-sm text-gray-100 p-1 mt-6 mb-4">
-        Descargar CV
-      </div>
+      </div>{" "}
     </div>
   );
 }
--- src/components/Tittle.jsx
+++ src/components/Tittle.jsx
@@ -4,19 +4,19 @@ function Tittle() {
   return (
     <div className=" flex flex-col h-40">
       <div>
-        <div className=" w-48 h-48 absolute ml-96 mt-12 text-transparent border-solid border-2 border-violet-600">
+        <div className=" w-48 h-48 absolute mt-12 ml-24 text-transparent border-solid border-2 border-violet-600">
           PHOTO
         </div>
-        <div className=" bg-blue-800 w-48 h-48 absolute rounded-full ml-96 mt-12 text-transparent ">
-          PHOTO
+        <div className=" bg-blue-800 w-48 h-48 absolute rounded-full ml-24 mt-12 text-transparent ">
+          
         </div>
-        <div className=" w-80 h-20 absolute -z-20 ml-80 mt-0  bg-gray-950 text-transparent"> {/*-z-20 es para superpones contenedores*/}
+        <div className=" w-80 h-20 absolute -z-20 m-10 mt-0  bg-gray-950 text-transparent"> {/*-z-20 es para superpones contenedores*/}
           PHOTO
         </div>
       </div>
       <div className="w-full flex justify-center h-40 my-20 p-6 pl-0 bg-blue-900 text-cyan-50 ">
         <div className=" text-gray-100 text-4xl justify-start start ">
-          <div className="flex flex-col ml-16 w-52">
+          <div className="flex flex-col ml-36 w-52">
             <p className="font-extrabold">REINEIRO</p>
             <p className="pl-12 font-extrabold">MONTERROSA</p>
           </div>
