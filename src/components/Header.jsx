31846629023182ab4f040591594b68ac62f33d1a
Rey <reydavidquiroz@gmail.com>
2024-04-14 16:00:59 -0500

    update header-2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { GrLinkedin } from "react-icons/gr";
 
 function Header() {
   return (
-    <div className="w-full flex justify-around items-center px-7 pr-3">
+    <div className="w-full flex justify-around items-center px-7 pr-3 mb-10">
       <div className=" flex items-start justify- cursor-pointer  my-6  w-12 h-12">
         <GrLinkedin className="flex justify-center items-center text-indigo-600 absolute rounded-lg bg hover:bg-gray-700 cursor-pointer w-12 h-12  text-3xl ml-6 " />
       </div>
