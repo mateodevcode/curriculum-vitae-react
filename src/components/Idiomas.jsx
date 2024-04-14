99b00f42a732a5c69df341a9d10b9a2714a25814
Mateo Lizcano <mateodevcode@gmail.com>
2024-04-14 13:39:04 +0200

    Se actualizo el componente de idiomas y se agrego el nivel del idioma

--- src/components/Idiomas.jsx
+++ src/components/Idiomas.jsx
@@ -12,61 +12,68 @@ const Idiomas = () => {
         IDIOMAS
       </h3>
       <div className="flex flex-row justify-around items-center">
-        <div className="w-20 flex flex-col justify-center items-center">
-            <span className="my-3 text-2xl uppercase font-semibold">Español</span>
-          <CircularProgressbar
-            value={percentageEspanol}
-            text={`${percentageEspanol}%`}
-            styles={buildStyles({
-              rotation: 0.25,
-              strokeLinecap: "butt",
-              textSize: "20px",
-              pathTransitionDuration: 0.5,
-              pathColor: `rgba(29, 78, 216)`,
-              textColor: "#000",
-              trailColor: "#b5b5b5",
-            //   backgroundColor: "#ff0",
-            })}
-          />
+        <div className="w-56 flex flex-col justify-center items-center ">
+          <span className="my-3 text-2xl uppercase font-semibold">Español</span>
+          <div className="w-20">
+            <CircularProgressbar
+              value={percentageEspanol}
+              text={`${percentageEspanol}%`}
+              styles={buildStyles({
+                rotation: 0,
+                strokeLinecap: "butt",
+                textSize: "20px",
+                pathTransitionDuration: 0.5,
+                pathColor: `rgba(29, 78, 216)`,
+                textColor: "#000",
+                trailColor: "#b5b5b5",
+                //   backgroundColor: "#ff0",
+              })}
+            />
+          </div>
+          <span className="my-3 text-2xl font-semibold text-gray-400">Nativo</span>
         </div>
 
-        <div className="w-20 flex flex-col justify-center items-center">
-            <span className="my-3 text-2xl uppercase font-semibold">Ingles</span>
-          <CircularProgressbar
-            value={percentageingles}
-            text={`${percentageingles}%`}
-            styles={buildStyles({
-              rotation: 0,
-              strokeLinecap: "butt",
-              textSize: "20px",
-              pathTransitionDuration: 0.5,
-              pathColor: `rgba(29, 78, 216)`,
-              textColor: "#000",
-              trailColor: "#b5b5b5",
-            //   backgroundColor: "#ff0",
-            })}
-          />
+        <div className="w-56 flex flex-col justify-center items-center">
+          <span className="my-3 text-2xl uppercase font-semibold">Ingles</span>
+          <div className="w-20">
+            <CircularProgressbar
+              value={percentageingles}
+              text={`${percentageingles}%`}
+              styles={buildStyles({
+                rotation: 0,
+                strokeLinecap: "butt",
+                textSize: "20px",
+                pathTransitionDuration: 0.5,
+                pathColor: `rgba(29, 78, 216)`,
+                textColor: "#000",
+                trailColor: "#b5b5b5",
+                //   backgroundColor: "#ff0",
+              })}
+            />
+          </div>
+          <span className="my-3 text-2xl font-semibold text-gray-400">B2: Intermedio alto</span>
         </div>
 
-        <div className="w-20 flex flex-col justify-center items-center">
-            <span className="my-3 text-2xl uppercase font-semibold">Aleman</span>
-          <CircularProgressbar
-            value={percentagealeman}
-            text={`${percentagealeman}%`}
-            styles={buildStyles({
-              rotation: 0,
-              strokeLinecap: "butt",
-              textSize: "20px",
-              pathTransitionDuration: 0.5,
-              pathColor: `rgba(29, 78, 216)`,
-              textColor: "#000",
-              trailColor: "#b5b5b5",
-            //   backgroundColor: "#ff0",
-            })}
-          />
+        <div className="w-56 flex flex-col justify-center items-center">
+          <span className="my-3 text-2xl uppercase font-semibold">Aleman</span>
+          <div className="w-20">
+            <CircularProgressbar
+              value={percentagealeman}
+              text={`${percentagealeman}%`}
+              styles={buildStyles({
+                rotation: 0,
+                strokeLinecap: "butt",
+                textSize: "20px",
+                pathTransitionDuration: 0.5,
+                pathColor: `rgba(29, 78, 216)`,
+                textColor: "#000",
+                trailColor: "#b5b5b5",
+                //   backgroundColor: "#ff0",
+              })}
+            />
+          </div>
+          <span className="my-3 text-2xl font-semibold text-gray-400">A1: Basico</span>
         </div>
-
-
       </div>
     </div>
   );
