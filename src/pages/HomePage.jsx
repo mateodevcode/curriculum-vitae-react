e0359a2c58733e83af2186534bb10a22ec691195
Rey <reydavidquiroz@gmail.com>
2024-04-16 08:14:57 -0500

    all page edit resposnsive Header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,23 +5,18 @@ import Idiomas from "../components/Idiomas";
 import Skills from "../components/Skills";
 import Target from "../components/Target";
 import Tittle from "../components/Tittle";
-import Header from "../components/Header"
-
+import Header from "../components/Header";
 
 function HomePage() {
-
   return (
     <div id="homepage">
-<<<<<<< HEAD
-    <Header />
-      <div className="flex flex-col justify-start items-center px-0 sm:px-10 md:px-20 lg:px-64">
-=======
-    {/* <Header /> */}
-      <div className={`flex flex-col justify-start items-center lg:px-64 md:px-20 sm:px-0`}>
->>>>>>> 7e2f90ba9806dd65071be8c731c0062d17fd7b63
+      <Header />
+      <div
+        className={`flex flex-col justify-start items-center lg:px-64 md:px-20 sm:px-0`}
+      >
         <div className="w-full">
-          {/* <Tittle />   */}
-          {/* <Target /> */}
+          <Tittle />
+          <Target />
           <HistoryDeploy />
           <Skills />
           <Idiomas />
